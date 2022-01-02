6ee219c3d15287abf3c7416f79a430057118245e
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-01-02 16:16:53 +0530

    Time: 157 ms (5.07%), Space: 46.7 MB (26.97%) - LeetHub

---
+++ contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if(nums.size()==0)
+        {
+            return false;
+        }
+        sort(nums.begin(),nums.end());
+        int check = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i] == check)
+            {
+                return true;
+                
+            }
+            else{
+               check = nums[i];
+            }
+        }
+        return false;
+    }
+};
