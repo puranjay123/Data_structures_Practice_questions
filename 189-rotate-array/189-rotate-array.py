eba43b05d239824c438cdc84be03b8496d48bca8
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-06-18 19:40:32 +0530

    Time: 310 ms (57.35%), Space: 25.3 MB (75.17%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,13 +1,33 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
+        
+#         kuch samajh nahi aya
+        k=k%len(nums)
+        count =0
+        start =0
+        while count<len(nums):
+            current =start
+            prev = nums[start]
+            
+            while True:
+                next = (current+k)% len(nums)
+                temp = nums[next]
+                nums[next]=prev
+                prev = temp
+                current = next
+                count +=1
+                if start == current:
+                    break
+            start +=1
         """
         Do not return anything, modify nums in-place instead.
         """
-        a = [0] * len(nums)
-        for i in range(len(nums)):
-            a[(i+k)% len(nums)] = nums[i]
-        for i in range(len(nums)):
-            nums[i] = a[i]
+        
+        # a = [0] * len(nums)
+#         for i in range(len(nums)):
+#             a[(i+k)% len(nums)] = nums[i]
+#         for i in range(len(nums)):
+#             nums[i] = a[i]
             
             
 #             3%7
