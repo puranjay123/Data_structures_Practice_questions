3671938263eb334574be2e27aefb44916d48c264
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-07-15 17:01:54 +0530

    Time: 57 ms (65.10%), Space: 14 MB (30.27%) - LeetHub

--- 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.py
@@ -5,19 +5,30 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        if head is None or head.next is None:
+            return head
+        if head.next.val != head.val:
+            head.next =self.deleteDuplicates(head.next)
+            return head
         cur = head
-        dummy = ListNode(next=head)
-        prev = dummy
+        
+        while cur.next and cur.next.val == cur.val:
+            cur = cur.next
+        return self.deleteDuplicates(cur.next)
+#         cur = head
+#         dummy = ListNode(next=head)
+#         prev = dummy
 
-        while cur and cur.next:
-            if cur.val!=cur.next.val:
-                prev =cur
-                cur = cur.next
-            else:
-                while cur.next and cur.val == cur.next.val:
-                    cur =cur.next
+#         while cur and cur.next:
+#             if cur.val!=cur.next.val:
+#                 prev =cur
+#                 cur = cur.next
+#             else:
+#                 while cur.next and cur.val == cur.next.val:
+#                     cur =cur.next
                     
-                prev.next = cur.next
-                cur = cur.next
-        return dummy.next
+#                 prev.next = cur.next
+#                 cur = cur.next
+#         return dummy.next
         
