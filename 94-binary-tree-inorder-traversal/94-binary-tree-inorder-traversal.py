e7664ec5c800e3f2bc3ba50ffb53df45c8add8e7
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-07-20 19:09:59 +0530

    Time: 60 ms (17.12%), Space: 13.8 MB (59.35%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -6,20 +6,30 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        
+        res = []
+        self.helper(root,res)
+        return res
+    def helper(self,root,res):
+        if root:
+            self.helper(root.left,res)
+            res.append(root.val)
+            self.helper(root.right,res)
+#        ***************Iterative Approach**********
 #         INorder tarversal
 #  left-> root->right
 # when we use stack order should be reversed
-# right->root->left
-        res,stack = [],[(root,False)]
-        while stack:
-            node,visited = stack.pop()
-            if node:
-                if visited:
-                    res.append(node.val)
-                else:
-                    stack.append((node.right,False))
-                    stack.append((node,True))
-                    stack.append((node.left,False))
-        return res
+# # right->root->left
+#         res,stack = [],[(root,False)]
+#         while stack:
+#             node,visited = stack.pop()
+#             if node:
+#                 if visited:
+#                     res.append(node.val)
+#                 else:
+#                     stack.append((node.right,False))
+#                     stack.append((node,True))
+#                     stack.append((node.left,False))
+#         return res
+
+
     
