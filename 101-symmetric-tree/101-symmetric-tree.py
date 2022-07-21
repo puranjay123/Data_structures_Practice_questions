dce123f860e6b535f3fe64a13bbc58adfdc38e28
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-07-22 02:45:12 +0530

    Time: 61 ms (31.19%), Space: 14 MB (60.43%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.py
+++ 101-symmetric-tree/101-symmetric-tree.py
@@ -6,18 +6,25 @@
 #         self.right = right
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        if not root :
-            return True
-        else:
-            return self.ismirror(root.left,root.right)
-    def ismirror(self,left,right):
-        if left is None and right is None:
-            return True
-        if left is None or right is None:
+        def isSym(L,R):
+            if not L and not R: 
+                return True
+            if L and R and L.val == R.val:
+                return isSym(L.left,R.right) and isSym(L.right,R.left)
             return False
-        if left.val == right.val:
-            outpair = self.ismirror(left.left,right.right)
-            inpair  = self.ismirror(left.right,right.left) 
-            return outpair and inpair
-        else:
-            return False
+        return isSym(root,root)
+    #     if not root :
+    #         return True
+    #     else:
+    #         return self.ismirror(root.left,root.right)
+    # def ismirror(self,left,right):
+    #     if left is None and right is None:
+    #         return True
+    #     if left is None or right is None:
+    #         return False
+    #     if left.val == right.val:
+    #         outpair = self.ismirror(left.left,right.right)
+    #         inpair  = self.ismirror(left.right,right.left) 
+    #         return outpair and inpair
+    #     else:
+    #         return False
