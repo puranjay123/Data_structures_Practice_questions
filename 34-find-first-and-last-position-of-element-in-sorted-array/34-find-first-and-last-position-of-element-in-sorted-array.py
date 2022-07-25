d1577f2ea649b37821776a5206efff3dd3d95be0
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-07-25 22:52:53 +0530

    Time: 96 ms (83.25%), Space: 15.4 MB (93.21%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,23 +1,56 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def binarySearchLeft(A,x):
-            left,right = 0,len(A)-1
-            while left<=right:
-                mid = (right+left)//2
-                if x>A[mid]:
-                    left = mid+1
-                else:
-                    right = mid-1
-            return left
-        def binarySearchRight(A,x):
-            left,right=0,len(A)-1
-            while left<=right:
-                mid = (left+right)//2
-                if x>=A[mid]:
-                    left = mid+1
-                else:
-                    right = mid-1
-            return right
-        left,right = binarySearchLeft(nums,target),binarySearchRight(nums,target)
-        return(left,right) if left<=right else[-1,-1]
+        result = [-1,-1]
+        
+        result[0] = self.findStaringIndex(nums,target)
+        result[1] = self.findEndIndex(nums,target)
+        return result
+    def findStaringIndex(self,nums,target):
+        index = -1
+        low,high = 0,len(nums)-1
+        
+        while low<=high:
+            mid = low +(high-low)//2
+            if nums[mid] == target:
+                index = mid
+                high = mid-1
+            elif nums[mid]>target:
+                high = mid -1
+            else:
+                low = mid+1
+        return index
+    def findEndIndex(self,nums,target):
+        index = -1
+        low,high = 0,len(nums)-1
+        while low<=high:
+            mid = low+(high-low)//2
+
+            if nums[mid]== target:
+                index =mid
+                low= mid+1
+            elif nums[mid]>target:
+                high = mid-1
+            else:
+                low = mid + 1
+        return index
+            # def binarySearchLeft(A,x):
+#             left,right = 0,len(A)-1
+#             while left<=right:
+#                 mid = (right+left)//2
+#                 if x>A[mid]:
+#                     left = mid+1
+#                 else:
+#                     right = mid-1
+#             return left
+#         def binarySearchRight(A,x):
+#             left,right=0,len(A)-1
+#             while left<=right:
+#                 mid = (left+right)//2
+#                 if x>=A[mid]:
+#                     left = mid+1
+#                 else:
+#                     right = mid-1
+#             return right
+#         left,right = binarySearchLeft(nums,target),binarySearchRight(nums,target)
+#         return(left,right) if left<=right else[-1,-1]
         
