25be498fde76e5c1b6e86c06121db6e56d7e3291
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-19 17:42:29 +0530

    Time: 53 ms (51.26%), Space: 14.2 MB (35.80%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,25 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        subset = []
+        def dfs(i):
+            if i>=len(nums):
+                res.append(subset.copy())
+                return
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i+1)
+            
+            #decision NOt to include nums[i]
+            subset.pop()
+            dfs(i+1)
+            
+        dfs(0)
+        return res
+            
+            
+            
+                
+        
+        
