9d4acd4c1a9a6255e3f49610b35041fa4cf74bc0
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-23 14:16:56 +0530

    Time: 384 ms (97.59%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -4,6 +4,10 @@
 # where Not exists (select 1 from orders B where A.id = B.customerId)
 # approach2
 
+# select A.name as customers from customers A
+# left join orders B  on A.id = B.customerId
+# where B.customerId is  NULL
+
+# approach3
 select A.name as customers from customers A
-left join orders B  on A.id = B.customerId
-where B.customerId is  NULL
+where A.id NOT in (select b.customerId from orders B)
