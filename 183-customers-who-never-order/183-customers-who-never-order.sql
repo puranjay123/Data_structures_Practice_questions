413850dfcfeed21ce045f873ad64aa154f4b7bb0
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-23 14:14:58 +0530

    Time: 638 ms (45.80%), Space: 0B (100.00%) - LeetHub

--- 183-customers-who-never-order/183-customers-who-never-order.sql
+++ 183-customers-who-never-order/183-customers-who-never-order.sql
@@ -1,3 +1,9 @@
 # Write your MySQL query statement below
+# Approach 1
+# select A.name as customers from customers A
+# where Not exists (select 1 from orders B where A.id = B.customerId)
+# approach2
+
 select A.name as customers from customers A
-where Not exists (select 1 from orders B where A.id = B.customerId)
+left join orders B  on A.id = B.customerId
+where B.customerId is  NULL
