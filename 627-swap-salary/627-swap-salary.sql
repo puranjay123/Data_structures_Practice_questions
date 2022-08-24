ff1bc39e0e36264bb905c064fb1c8c12e6a9d81f
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-24 16:18:23 +0530

    Time: 438 ms (23.25%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,3 +1,4 @@
 # Write your MySQL query statement below
 # update salary set sex = CHAR(ASCII('f')^ ASCII('m')^ ASCII(sex));
-update salary set sex = (CASE when sex = 'm' then 'f' else 'm' end)
+# update salary set sex = (CASE when sex = 'm' then 'f' else 'm' end)
+update salary set sex = if (sex = 'f','m','f');
