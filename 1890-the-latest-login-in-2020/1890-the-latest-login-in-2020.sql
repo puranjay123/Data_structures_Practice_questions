f26e758a6c8c33dd8dbc8cc09b3a9fb0faec8bfd
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-30 17:24:58 +0530

    Time: 625 ms (69.21%), Space: 0B (100.00%) - LeetHub

---
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -0,0 +1,8 @@
+# # Write your MySQL query statement below
+# select distinct(user_id) ,time_stamp as last_stamp
+# from logins
+# where time_stamp>='2020-01-01 00:00:00' and time_stamp<='2020-12-31 23:59:59'
+# order by user_id
+select user_id,max(time_stamp) as last_stamp 
+from logins where year(time_stamp) =2020
+group by user_id
