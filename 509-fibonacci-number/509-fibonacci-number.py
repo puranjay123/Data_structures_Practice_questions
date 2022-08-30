a4ebcec9c40dd62c48d2d0651cfbb051adc0c9d4
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-30 19:19:59 +0530

    Time: 38 ms (83.08%), Space: 13.8 MB (54.26%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -4,10 +4,12 @@ class Solution:
             return 0
         if n==1:
             return 1
-        dp = [-1]*(n+1)
-        dp[0] = 0
-        dp[1] =1
+        # dp = [-1]*(n+1)
+        prev2 =0
+        prev =1
         for i in range(2,n+1):
-            dp[i] = dp[i-1]+dp[i-2]
-        return dp[n]
+            curi=prev2+prev
+            prev2 = prev
+            prev = curi
+        return prev
         
