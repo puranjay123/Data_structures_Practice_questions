02d75c9b585a54d86b1ea0d964e336dc5bbbeeaf
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-30 18:55:56 +0530

    Time: 35 ms (88.99%), Space: 13.9 MB (54.26%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,12 +1,12 @@
 class Solution:
-    
     def fib(self, n: int) -> int:
-        if(n<=1):
-            return n
-        return self.fib(n-1)+self.fib(n-2)
-        
-#         n= input("enter n")
-#         dp = []
-#         print(f(n,dp))
-
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        dp = [0]*(n+1)
+        dp[1] =1
+        for i in range(2,n+1):
+            dp[i] = dp[i-1]+dp[i-2]
+        return dp[n]
         
