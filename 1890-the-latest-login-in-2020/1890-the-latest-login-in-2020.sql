5a4f503e000c6fa730c017b942844b7be54e82cb
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-08-30 17:25:54 +0530

    Time: 931 ms (34.35%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -4,5 +4,5 @@
 # where time_stamp>='2020-01-01 00:00:00' and time_stamp<='2020-12-31 23:59:59'
 # order by user_id
 select user_id,max(time_stamp) as last_stamp 
-from logins where year(time_stamp) =2020
+from logins where time_stamp like '2020%'
 group by user_id
