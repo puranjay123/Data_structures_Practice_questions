3462515a5435f3867114f790dc01fbbd5f6bfdc9
Puranjay Kwatra <55429956+puranjay123@users.noreply.github.com>
2022-09-02 19:45:49 +0530

    Time: 40 ms (70.42%), Space: 13.8 MB (56.92%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,11 +1,22 @@
 class Solution:
     @cache
     def climbStairs(self, n: int) -> int:
-        #Approach1
         
+#         Apprach 2
         if n==1:
             return 1
-        if n==2:
-            return 2
-        return self.climbStairs(n-1)+self.climbStairs(n-2)
+        res = [0 for i in range(n)]
+        res[0]=1
+        res[1]=2
+        for i in range(2,n):
+            res[i] = res[i-1]+res[i-2]
+        return res[-1]
+        
+        #Approach1
+        
+        # if n==1:
+        #     return 1
+        # if n==2:
+        #     return 2
+        # return self.climbStairs(n-1)+self.climbStairs(n-2)
         
